f91beaaca3e0adadc3e46f034391f4b78a0278c3
meha786 <verma.me@husky.neu.edu>
2017-03-13 22:46:17 -0700

    Created bigHashMap that save the class name as key and its vocabulary length as value

--- ML_PS2/src/ReadTextFile.java
+++ ML_PS2/src/ReadTextFile.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -38,6 +39,133 @@ public class ReadTextFile {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static HashMap<Integer,Integer>  calculateClassVocab(){
+		int totalVocab = 0;
+		HashMap<Integer,Integer> bigMap = new HashMap<>();
+
+		String path1 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\articles.train.txt";
+		int articlesVocab = calculateClassVocabHelper(path1,totalVocab);
+		bigMap.put(1,articlesVocab);
+		totalVocab = 0;
+
+		String path2 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\corporate.train.txt";
+		int corporateVocab = calculateClassVocabHelper(path2,totalVocab);
+		bigMap.put(2,corporateVocab);
+		totalVocab = 0;
+
+		String path3 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\enron_t_s.train.txt";
+		int enron_t_sVocab = calculateClassVocabHelper(path3,totalVocab);
+		bigMap.put(3,enron_t_sVocab);
+		totalVocab = 0;
+
+		String path4 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\enron_travel_club.train.txt";
+		int enron_travel_club = calculateClassVocabHelper(path4,totalVocab);
+		bigMap.put(4,enron_travel_club);
+		totalVocab = 0;
+
+		String path5 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\hea_nesa.train.txt";
+		int hea_nesa = calculateClassVocabHelper(path5,totalVocab);
+		bigMap.put(5,hea_nesa);
+		totalVocab = 0;
+
+		String path6 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\personal.train.txt";
+		int personal = calculateClassVocabHelper(path6,totalVocab);
+		bigMap.put(6,personal);
+		totalVocab = 0;
+
+		String path7 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\systems.train.txt";
+		int systems = calculateClassVocabHelper(path7,totalVocab);
+		bigMap.put(7,systems);
+		totalVocab = 0;
+
+		String path8 = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\tw_commercial_group.train.txt";
+		int tw_commercial_group = calculateClassVocabHelper(path7,totalVocab);
+		bigMap.put(8,tw_commercial_group);
+		totalVocab = 0;
+
+		return bigMap;
+	}
+
+	public static int calculateClassVocabHelper(String path, int totalVocab){
+
+
+		try {
+			HashMap<String,Integer> vocabMap = new HashMap<>();
+			FileReader reader = new FileReader(path);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			String line;
+			while ((line = bufferedReader.readLine())!=null){
+				String[] splitLine = line.split(" ");
+				String key = splitLine[0];
+				String value = splitLine[1];
+				int valueInt = Integer.parseInt(value);
+				vocabMap.put(key,valueInt);
+			}
+			Set<String> keySet = vocabMap.keySet();
+			for(String s : keySet){
+				totalVocab = totalVocab + vocabMap.get(s);
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+		return totalVocab;
+	}
+
+
+
+
+	public static int calculateVocab(){
+		int totalVocab = 0;
+		try {
+			String path = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\vocabulary.txt";
+			HashMap<String,Integer> vocabMap = new HashMap<>();
+
+			FileReader reader = new FileReader(path);
+			BufferedReader bufferedReader = new BufferedReader(reader);
+			String line;
+
+			while ((line = bufferedReader.readLine())!=null){
+				String[] splitLine = line.split(" ");
+				String key = splitLine[0];
+				String value = splitLine[1];
+				int valueInt = Integer.parseInt(value);
+				vocabMap.put(key,valueInt);
+			}
+			Set<String> keySet = vocabMap.keySet();
+			for(String s : keySet){
+				totalVocab++;
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+
+		return totalVocab;
+	}
+
+
+
+
+
+	public static HashMap<String,Double> priorProbabilities(HashMap<String,ArrayList<Integer>> map){
+
+		HashMap<String,Double> prob = new HashMap<>();
+		Set<String> keySet = map.keySet();
+		double totalNumDoc = 0;
+
+		for(String classLabel : keySet){
+			double value = map.get(classLabel).size();
+			totalNumDoc = totalNumDoc + value;
+		}
+
+		for(String classLabel : keySet){
+			double probability = (map.get(classLabel).size())/totalNumDoc;
+			prob.put(classLabel,probability);
+		}
+
+		return prob;
 	}
 
 
@@ -109,20 +237,13 @@ public class ReadTextFile {
 	public static void writeOutputFile(){
 		
 		try {
-			
-			
 			String path = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\materials\\ps2\\data\\processed\\vocabulary.txt";
-
 			final String FILENAME = "C:\\Users\\Meha Verma\\Desktop\\New folder (5)\\output.txt";
-			
 			HashMap<String,Integer> content = countFrequency(path);
-			
 			
 			FileWriter fw = new FileWriter(FILENAME);
 			BufferedWriter bw = new BufferedWriter(fw);
-			
 			Set<String> keySet = content.keySet();
-
 			//System.out.println(content.size());
 			int i = 0;
 			for(String s : keySet){
@@ -134,9 +255,6 @@ public class ReadTextFile {
 			System.out.println(i);
 			
 			bw.close();
-			
-			
-
 			System.out.println("Done");
 
 		} catch (IOException e) {
@@ -160,8 +278,12 @@ public class ReadTextFile {
 		for(String s : keySet){
 			//System.out.println("key is: " + s + " Value is: " + hMap.get(s));
 		}
+		calculateClassVocab();
+		int vocabulary = calculateVocab();
 
 		HashMap<String, ArrayList<Integer>> map = readFileAndGiveMap();
+
+		HashMap<String, Double> map2 = priorProbabilities(map);
 			
 	}
 }
