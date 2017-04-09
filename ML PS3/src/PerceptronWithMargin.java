ee8b6a88869221547541f00fa5ea07698c3012f9
meha786 <verma.me@husky.neu.edu>
2017-04-09 00:37:22 -0700

    Generated w vector array for different values of eta

--- ML PS3/src/PerceptronWithMargin.java
+++ ML PS3/src/PerceptronWithMargin.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
  */
 public class PerceptronWithMargin {
 
+    static int largestXValue = Integer.MIN_VALUE;
+
     public static ArrayList<Row> readOutputFile() {
 
         ArrayList<Row> rowsList = new ArrayList<>();
@@ -42,7 +44,14 @@ public class PerceptronWithMargin {
                     String partX = xVectorArray[0];
                     Double partXDouble = Double.valueOf(partX);
                     xVectorEachRow.add(partXDouble);
+
+                    if(xVectorEachRow.size() > largestXValue)
+                    {
+                        largestXValue = xVectorEachRow.size();
+                    }
                 }
+
+
 
                 r.setXList(xVectorEachRow);
                 rowsList.add(r);
@@ -56,13 +65,84 @@ public class PerceptronWithMargin {
     }
 
 
+    private static ArrayList<Double> calculateWByPerceptron(ArrayList<Row> dataSetRows) {
+        ArrayList<Double> wVector = new ArrayList<>();
 
+        for (int i=0;i<largestXValue;i++)
+        {
+            wVector.add(0.0);
+        }
+        double b = 0;
+        int t = 0;
+        int T = 20;
+
+        int gama = 1;
+        double eta[] = new double[]{1.5, 0.25, 0.03, 0.005, 0.001};
+
+
+        for (int e=0; e<eta.length;e++)
+        {
+            for (t=0;t<T;t++)
+            {
+                for (Row r : dataSetRows)
+                {
+                    int y = r.getLabel();
+
+                    ArrayList<Double> xVector = r.getXList();
+
+                    double sumOfProduct = 0;
+                    for (int i=0;i<xVector.size();i++)
+                    {
+                        Double w = wVector.get(i);
+                        Double xValue = xVector.get(i);
+
+                        sumOfProduct = sumOfProduct + (w * xValue);
+                    }
+
+
+                    if(( y * (sumOfProduct + b)) <= gama)
+                    {
+                        for(int i=0;i<xVector.size();i++)
+                        {
+
+                            double newValue =  wVector.get(i) + (eta[e] * (xVector.get(i)) * y);
+                            wVector.set(i,newValue);
+                            b = b + eta[e] * y;
+
+                        }
+                    }
+
+                }
+
+            }
+
+            System.out.println("wVector with " + eta[e] );
+
+            for (int i=0; i<wVector.size();i++)
+            {
+                System.out.print(wVector.get(i));
+            }
+
+            System.out.println("\n");
+
+        }
+
+        return wVector;
+
+    }
 
 
     public static void main(String args[])
     {
+
         ArrayList<Row> dataSetD1 = readOutputFile();
 
+        ArrayList<Double> learnedWVector = calculateWByPerceptron(dataSetD1);
+
+        System.out.println();
+
     }
+
+
 
 }
